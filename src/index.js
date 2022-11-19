daaad128adb54932dc035c6e787d7edb1b043806
Quentin ANIERE <quentin.aniere@gmail.com>
2022-11-19 17:22:25 -0500

    Finish backend

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser'); //For parsing the post request data
 const register = require('./users/register'); //The register function
 const login = require('./users/login'); //The login function
 
+const {newEvent, getEvents, deleteEvent} = require('./events'); //Events related functions
+const {addTodo, getTodos, markDone} = require('./todos'); //Todos related functions
 
 const app = express(); //Instanciate the express app
 app.set('view engine', 'ejs');
@@ -76,6 +78,35 @@ app.post('/api/login', (req, res) => {
     login(req, res, database);
 });
 
+app.post('/api/event/new', (req, res) => {
+//When a form is posted at http://localhost:3000/api/event/new')
+    newEvent(req, res, database);
+});
+
+app.post('/api/event/get', (req, res) => {
+//When a form is posted at http://localhost:3000/api/event/get
+    getEvents(req, res, database);
+});
+
+app.post('/api/event/delete', (req, res) => {
+//When a form is posted at http://localhost:3000/api/event/delete
+    deleteEvent(req, res, database);
+});
+
+app.post('/api/todo/add', (req, res) => {
+//When a form is posted at http://localhost:3000/api/todo/add
+    addTodo(req, res, database);
+});
+
+app.post('/api/todo/get', (req, res) => {
+//When a form is posted at http://localhost:3000/api/todo/get
+    getTodos(req, res, database);
+});
+
+app.post('/api/todo/done', (req, res) => {
+//When a form is posted at http://localhost:3000/api/todo/done
+    markDone(req, res, database);
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
