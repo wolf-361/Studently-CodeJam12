58391e017776fc4a5d9427324b086f152b6be903
Janik Lesage <janik.lesage@uqtr.ca>
2022-11-19 22:42:50 -0500

    background

---
+++ Background_JS.js
@@ -0,0 +1,1 @@
+document.querySelector('div.image').style.backgroundImage ="url('https://www.befunky.com/images/prismic/5ddfea42-7377-4bef-9ac4-f3bd407d52ab_landing-photo-to-cartoon-img5.jpeg?auto=avif,webp&format=jpg&width=863')";
